b82bdd78762667ca189e3a81f7044a80901d19ea
Mulyar Vlad <mulyarvlad@MulyarVlad.local>
2023-02-21 21:28:34 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+const array = [1, 2, 3, 4, 5, 6, 7];
+removeElement(array, 5 );
+console.log(array);
+// Результат: [1, 2, 3, 4, 6, 7]
+
+function removeElement(array, item) {
+    array.splice(item - 1, 1);
+    console.log(array);
+}
