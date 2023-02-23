07046f1901dbea00be15ef0e304ada63dfa5f02c
Mulyar Vlad <mulyarvlad@MulyarVlad.local>
2023-02-23 15:26:26 +0200

    second commit

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const array = [1, 2, 3, 4, 5, 6, 7];
-removeElement(array, 5 );
-console.log(array);
-// Результат: [1, 2, 3, 4, 6, 7]
+// const array = [1, 2, 3, 4, 5, 6, 7];
+// removeElement(array, 5 );
+// console.log(array);
+// // Результат: [1, 2, 3, 4, 6, 7]
 
-function removeElement(array, item) {
-    array.splice(item - 1, 1);
-    console.log(array);
+let array = [1, 2, 3, 4, 5, 6, 7];
+console.log(array);
+
+function removeElement(array, index){
+    array.splice(index, 1);
+    return array
 }
+console.log(removeElement(array, 4));
